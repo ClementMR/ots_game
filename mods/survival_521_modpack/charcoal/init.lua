23e7f781af5996c62f0ab618969f4e71ffe77c65
Clément Marron <marronclement0403@gmail.com>
2025-03-25 00:11:51 +0100

    Added charcoal block & recipe

--- mods/survival_521_modpack/charcoal/init.lua
+++ mods/survival_521_modpack/charcoal/init.lua
@@ -3,6 +3,36 @@ core.register_craftitem("charcoal:charcoal", {
     inventory_image = "default_coal_lump.png",
 })
 
+core.register_node("charcoal:charcoalblock", {
+	description = "Charcoal Block",
+	tiles = {"default_coal_block.png"},
+	is_ground_content = false,
+	groups = {cracky = 3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+core.register_craft({
+	output = "charcoal:charcoalblock",
+	recipe = {
+		{"charcoal:charcoal", "charcoal:charcoal", "charcoal:charcoal"},
+		{"charcoal:charcoal", "charcoal:charcoal", "charcoal:charcoal"},
+		{"charcoal:charcoal", "charcoal:charcoal", "charcoal:charcoal"},
+	}
+})
+
+core.register_craft({
+	output = "charcoal:charcoal 9",
+	recipe = {
+		{"charcoal:charcoalblock"},
+	}
+})
+
+core.register_craft({
+	type = "fuel",
+	recipe = "charcoal:charcoalblock",
+	burntime = 375,
+})
+
 core.register_craft({
 	type = "cooking",
 	output = "charcoal:charcoal",
